9161abc573b1a39e66ef8531d024e5a800390b34
garimaj <garimaj7190@gmail.com>
2014-07-21 15:45:32 +0530

    adding files

---
+++ ToDoMon0_0_11/DataModel/CheckPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoMon0_0_11.DataModel
+{
+    class CheckPoint
+    {
+        public int nextChekPoint { get; set; }
+        public int type { get; set; }
+        public int id { get; set; }
+    }
+}
---
+++ ToDoMon0_0_11/DataModel/Tasko.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoMon0_0_11.DataModel
+{
+    public class Task
+    {
+        public int id { get; set; }
+        public String Title { get; set; }
+        public Double completionPercentage { get; set; }
+        public int priority { get; set; }
+
+        public int personMinutes { get; set; }
+        public int startBy { get; set; }
+        //CheckPoint deadline { get; set; }
+        public int workPressure { get; set; }
+        public int type { get; set; }
+        public bool recurring { get; set; }
+        public int imageType { get; set; }
+        public String taskImagePath { get; set; }
+        public bool openIdea { get; set; }
+
+
+
+
+    }
+}
